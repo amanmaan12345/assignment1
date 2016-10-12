df51ff631afaa4348fd3d2f94a327eed09d27bec
amanmaan12345 <200325829@student.georgianc.on.ca>
2016-10-12 16:26:28 -0400

    commit

--- routes/index.js
+++ routes/index.js
@@ -12,20 +12,20 @@ router.get('/aboutMe', function(req, res, next) {
   res.render('aboutMe', { title: 'About Me',
     heading: 'About' });
 });
-
+//get project page
 router.get('/projectsPage', function(req, res, next) {
   res.render('projectsPage', { title: 'Projects',
     heading: 'Some project and Acheivements' });
 });
-
+//get contact me page
 router.get('/contactMe', function(req, res, next) {
   res.render('contactMe', { title: 'Projects',
     heading: 'Contact Information' });
 });
-
+//get services page
 router.get('/servicesPage', function(req, res, next) {
   res.render('servicesPage', { title: 'Services',
-    heading: 'Services offered ' });
+    heading: 'Services offered' });
 });
 
 

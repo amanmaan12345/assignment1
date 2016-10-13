4ef7ec0690345383b0db005564e6c6cd631dfd1d
amanmaan12345 <200325829@student.georgianc.on.ca>
2016-10-13 15:36:52 -0400

    end of assignment1

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,8 @@
+/*
+* File name: index.js
+* Author Name: Amanjot Singh
+* Description: this javascript file controls thelinks of the website
+* **/
 var express = require('express');
 var router = express.Router();
 
@@ -10,7 +15,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/aboutMe', function(req, res, next) {
   res.render('aboutMe', { title: 'About Me',
-    heading: 'About' });
+    heading: 'About Me' });
 });
 //get project page
 router.get('/projectsPage', function(req, res, next) {
@@ -19,7 +24,7 @@ router.get('/projectsPage', function(req, res, next) {
 });
 //get contact me page
 router.get('/contactMe', function(req, res, next) {
-  res.render('contactMe', { title: 'Projects',
+  res.render('contactMe', { title: 'Contact Me',
     heading: 'Contact Information' });
 });
 //get services page
